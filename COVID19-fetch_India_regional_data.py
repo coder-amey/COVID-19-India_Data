981f098fbb4e2fb14704836a5aea1cecb83f218c
Amey Noolkar <fb4amey@gmail.com>
2020-03-29 16:51:46 +0530

    Created the repo with first two datasets.

---
+++ COVID19-fetch_India_regional_data.py
@@ -0,0 +1,29 @@
+'''This script scrapes the home page of the MoHFW, Govt. of India for latest updates for COVID-data and stores the last tally in a CSV file with the corresponding date in the filename. It also updates the time-series dataset with the latest available data and regenerates the corresponding CSV and JSON files.'''
+
+import pandas as data
+import numpy as maths
+from datetime import datetime
+
+rows = 27	#The number of rows to be fetched from the table (this may change).
+date = datetime.now().strftime("%d-%m-%Y") #Date of update.
+
+#Scrape the HTML tables, load the last table, remove unwanted columns and rename and reformat the columns. Add the "National Total" row.
+tables = data.read_html("https://www.mohfw.gov.in/")
+updated_tally = tables[-1][:rows].drop(["S. No.", "Total Confirmed cases ( Foreign National )"], axis = 1)
+updated_tally = updated_tally.rename(columns = {"Name of State / UT": "Region", "Total Confirmed cases (Indian National)": "Confirmed", "Cured/Discharged/Migrated": "Recovered/Migrated", "Death": "Deceased"})
+updated_tally = updated_tally.astype({"Confirmed": int, "Recovered/Migrated": int, "Deceased": int})
+updated_tally.loc[:, "Confirmed"] = updated_tally["Confirmed"] + tables[-1][:rows]["Total Confirmed cases ( Foreign National )"].astype(int)
+updated_tally = updated_tally.append(updated_tally.sum(numeric_only = True), ignore_index = True)
+updated_tally.iloc[-1, 0] = "National Total"
+
+#Store the dataset to a CSV file.
+updated_tally.to_csv("datasets/India_regional_aggregated_{}.csv".format(date), index = False)
+
+#Insert a "Date" column to the data frame and merge it with the time-series.
+updated_tally.insert(1, "Date", date)
+time_series = data.read_csv("time-series/India_regional_aggregated.csv")		#Load the time-series.
+time_series = time_series[time_series["Date"] != date]	#Discard other updates made today.
+time_series = data.concat([time_series, updated_tally])
+
+#Rewrite the updated time-series to its CSV file.
+time_series.to_csv("time-series/India_regional_aggregated.csv", index = False)
