e92195cd02fee3887969a802dfbb73ee495bcd05
Amey Noolkar <fb4amey@gmail.com>
2020-04-20 11:41:04 +0530

    Automatic fetch and update.

--- update_scripts/COVID19-fetch_India_regional_data.py
+++ update_scripts/COVID19-fetch_India_regional_data.py
@@ -19,7 +19,7 @@ updated_tally.iloc[-1, 0] = "National Total"
 
 #Correct the errors in the table.
 updated_tally.loc[updated_tally.Region == "Telengana", "Region"] = "Telangana"		#Correct the spelling of Telangana.
-updated_tally = updated_tally[updated_tally.Region != "Nagaland#"]		#Discard the entry of Nagaland with '0' cases.
+updated_tally = updated_tally[updated_tally.Region != "Nagaland"]		#Discard the entry of Nagaland with '0' cases.
 
 #Store the dataset to a CSV file.
 updated_tally.to_csv(base_dir + "datasets/India_regional_aggregated_{}.csv".format(date), index = False)
