49b472a8d6fd51c73d74b566d66f182440acfce0
Amey Noolkar <fb4amey@gmail.com>
2020-06-11 12:02:47 +0530

    Merged separate entries for DNHDD. Reordered columns.

--- update_scripts/fetch_daily_update.py
+++ update_scripts/fetch_daily_update.py
@@ -5,7 +5,7 @@ import numpy as maths
 import os.path
 from datetime import datetime
 
-rows = 35	#The number of rows to be fetched from the table (this may change).
+rows = 36	#The number of rows to be fetched from the table (this may change).
 base_dir = os.path.join(os.path.dirname(__file__), "../")		#Obtain the path to the base directory for absosulte addressing.
 date = datetime.now().strftime("%d-%m-%Y") 	#Date of update.
 
@@ -14,13 +14,20 @@ tables = data.read_html("https://www.mohfw.gov.in/")
 updated_tally = tables[-1][:rows].drop(["S. No.", "Active Cases*"], axis = 1)
 updated_tally = updated_tally.rename(columns = {"Name of State / UT": "Region", "Total Confirmed cases*": "Confirmed", "Cured/Discharged/Migrated*": "Recovered/Migrated", "Deaths**": "Deceased"})
 
+#Reorder and format the columns.
+updated_tally = updated_tally[["Region", "Confirmed", "Recovered/Migrated", "Deceased"]]
 updated_tally = updated_tally.astype({"Confirmed": int, "Recovered/Migrated": int, "Deceased": int})
 updated_tally = updated_tally.append(updated_tally.sum(numeric_only = True), ignore_index = True)
 updated_tally.iloc[-1, 0] = "National Total"
 
 #Correct the errors in the table.
 updated_tally.loc[updated_tally.Region == "Telengana", "Region"] = "Telangana"		#Correct the spelling of Telangana.
+#Merge the rows belonging to Dadra and Nagar Haveli and Daman and Diu.
+df = updated_tally[updated_tally["Region"].isin(["Daman & Diu", "Dadar Nagar Haveli"])]		#Extract entries.
+#Sum and merge the entries.
+updated_tally.loc[updated_tally.Region == "Dadar Nagar Haveli", ["Confirmed", "Recovered/Migrated", "Deceased"]] = df.sum(numeric_only = True).to_list()
 updated_tally.loc[updated_tally.Region == "Dadar Nagar Haveli", "Region"] = "Dadra and Nagar Haveli and Daman and Diu"		#Correct the spelling of DNHDD.
+updated_tally = updated_tally[updated_tally.Region != "Daman & Diu"]		#Drop the row for Daman and Diu.
 
 #Store the dataset to a CSV file.
 updated_tally.to_csv(base_dir + "datasets/India_regional_aggregated_{}.csv".format(date), index = False)
