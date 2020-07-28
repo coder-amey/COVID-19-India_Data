12e46ed121812ada062689e7cc5720169e005e63
Amey Noolkar <fb4amey@gmail.com>
2020-07-28 19:48:18 +0530

    Relocated the upstream source to its JSON resource.

--- update_scripts/fetch_daily_update.py
+++ update_scripts/fetch_daily_update.py
@@ -5,14 +5,14 @@ import numpy as maths
 import os.path
 from datetime import datetime
 
-rows = 35		#The number of rows to be fetched from the table (this may change).
 base_dir = os.path.join(os.path.dirname(__file__), "../")		#Obtain the path to the base directory for absosulte addressing.
 date = datetime.now().strftime("%d-%m-%Y") 	#Date of update.
 
-#Scrape the HTML tables, load the last table, remove unwanted columns and rename and reformat the columns. Add the "National Total" row.
-tables = data.read_html("https://www.mohfw.gov.in/")
-updated_tally = tables[-1][:rows].drop(["S. No.", "Active Cases*"], axis = 1)
-updated_tally = updated_tally.rename(columns = {"Name of State / UT": "Region", "Total Confirmed cases*": "Confirmed", "Cured/Discharged/Migrated*": "Recovered/Migrated", "Deaths**": "Deceased"})
+#Tabulate the JSON data. Remove unwanted columns and rename the rest. Drop the final total.
+updated_tally = data.read_json('https://www.mohfw.gov.in/data/datanew.json', orient = 'records')
+updated_tally = updated_tally.drop(['sno', 'active', 'positive', 'cured', 'death', 'new_active', 'state_code'], axis = 1)
+updated_tally = updated_tally.rename(columns = {"state_name": "Region", "new_positive": "Confirmed", "new_cured": "Recovered/Migrated", "new_death": "Deceased"})
+updated_tally = updated_tally[updated_tally.Region != ""]
 
 #Reorder and format the columns.
 updated_tally = updated_tally[["Region", "Confirmed", "Recovered/Migrated", "Deceased"]]
@@ -21,7 +21,7 @@ updated_tally = updated_tally.append(updated_tally.sum(numeric_only = True), ign
 updated_tally.iloc[-1, 0] = "National Total"
 
 #Correct the errors in the table.
-updated_tally.loc[updated_tally.Region == "Telangana***", "Region"] = "Telangana"		#Correct the spelling of Telangana.
+#updated_tally.loc[updated_tally.Region == "Telangana***", "Region"] = "Telangana"		#Correct the spelling of Telangana.
 
 #Store the dataset to a CSV file.
 updated_tally.to_csv(base_dir + "datasets/India_regional_aggregated_{}.csv".format(date), index = False)
